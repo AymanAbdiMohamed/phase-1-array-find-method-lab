872649661a3a8be02e5b132505d6ca8677a50c56
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-09-25 08:37:00 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+function findSuperbowlWin(records) {
+  const winRecord = records.find(record => record.result === "W" && record.team === "Denver Broncos");
+  return winRecord ? winRecord.year : undefined;
+}
+import { findSuperbowlWin } from './index.js';  // Adjust the name here
